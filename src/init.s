c7e392613717dc641a0deae0ceecb66efabcb420
Levente Bajczi <bajczilevi@gmail.com>
2017-11-08 00:05:44 +0000

    added initialization

--- src/init.s
+++ src/init.s
@@ -2,49 +2,77 @@
 
 .data
 addr:
-    .long 0x0
+    .quad 0x0
 len:
-    .long 0x0
+    .quad 0x0
 
 .text
 init:
-    push %r15
-    push %r14
-
-    mov $addr, %r15         #address of the addr global variable
-    mov $addr, %r14         #address of the len global variable
-
-    call allocate_safemem
-    call test_null
-
-    pop %r14
-    pop %r15
-    ret
+    jmp allocate_safemem
 
 allocate_safemem:
-    push %rsi
-    MOV %rdi, %rsi      #size of the to-be-allocated memory
-    push %rdi
-    XOR %rdi, %rdi      #address of the to-be-allocated memory, NULL (no preference)
-    push %rdx
-    MOV $0x2, %rdx      #0babc -> a: EXEC, b: WRITE, c: READ (prot), here we only want to be able to write into memory
+
+    MOV %rdi, len(,1)   #update len
+
     push %r10
-    MOV $0x22,%r10      #flags, here private (0x02) and anonymus (0x20), so that it doesn't get swapped to unsafe diskspace 
     push %r8
-    MOV $1,   %r8       #filedescriptor of output, 1 is the standard output
     push %r9
+
+    MOV %rdi, %rsi      #size of the to-be-allocated memory
+    XOR %rdi, %rdi      #address of the to-be-allocated memory, NULL (no preference)
+    MOV $0x2, %rdx      #0babc -> a: EXEC, b: WRITE, c: READ (prot), here we only want to be able to write into memory
+    MOV $0x22,%r10      #flags, here private (0x02) and anonymus (0x20), so that it doesn't get swapped to unsafe diskspace 
+    MOV $1,   %r8       #filedescriptor of output, 1 is the standard output
     xor %r9,  %r9       #null the offset, we want the entire space
-    push %rax
     MOV $9,   %rax      #sys_mmap is the 9th system call in the linux x64 kernel
     syscall 
 
-    MOV %rax, (%r15)    #update addr
-    MOV %rdi, (%r14)    #update len
+    MOV %rax, addr(,1)  #update addr
     
-    pop %rax
-    pop %r9
-    pop %r8
     pop %r10
-    pop %rdx
-    pop %rdi
-    pop %rsi
+    pop %r8
+    pop %r9
+
+    jmp test_null
+test_null:
+
+    push %r15
+    push %r14
+    push %r13
+    push %r12
+
+    MOV addr, %r15
+
+    CMP $0x0, %r15              #if the address is still 0x00000000, there is a good reason to believe it didn't work
+    JE  test_bad
+
+    MOV len, %r14
+    MOV $0xffffffff, %r13       #store testdata in r13
+    xor %r12, %r12
+iteration:
+    ADD $8, %r12
+    CMP %r12, %r14              #all good
+    JLE test_good
+    MOV %r13, -8(%r15,%r12,1)   #Write the testdata to the last available address
+
+    MOV -8(%r15,%r12,1), %r11   #Reading back the data, checking for integrity
+    CMP %r11, %r13
+    JNE test_bad
+
+    JMP iteration
+test_bad:                       #Arrive here if something bad happened
+    pop %r15
+    pop %r14
+    pop %r13
+    pop %r12
+
+    MOV $0, %rax
+    ret
+test_good:                      #Arrive here if we have $len allocated bytes starting at $addr
+    pop %r15
+    pop %r14
+    pop %r13
+    pop %r12
+
+    MOV $1, %rax  
+    ret
--- src/test.s
+++ src/test.s
@@ -10,14 +10,14 @@
 .data
 .text
 _start:
-    call powmod 
-
-loop:
-    NOP
-    
-    JMP loop
-
+    push %rdi
+    MOV $0xf, %rdi #Size of the memory to allocate
+    INC %rdi
+    call init 
+    #cmp $1, %rax
+    #jne end              #if not 1, then something bad happened
 end:
+    JMP end
     mov $60, %rax
     xor %rdi, %rdi
     syscall
